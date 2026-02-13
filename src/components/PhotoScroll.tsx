083461621bc9abdf7e80f28dd864c24b1d438245
SPSKathiravan <spskathiravan2004@gmail.com>
2026-02-14 05:13:36 +0530

    Add a sixth photo entry to the PHOTOS array

--- src/components/PhotoScroll.tsx
+++ src/components/PhotoScroll.tsx
@@ -7,6 +7,8 @@ const PHOTOS = [
     { id: 3, src: '', alt: '' },
     { id: 4, src: '', alt: '' },
     { id: 5, src: '', alt: '' },
+    { id: 6, src: '', alt: '' },
+
 ];
 
 export default function PhotoScroll() {
